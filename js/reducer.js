004afa898d4e76a180d7a98ec044da85c4b69659
zdurr22 <zdurr22@gmail.com>
2021-11-09 13:05:11 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -7,7 +7,19 @@ function changeState(state, action){
   }
 }
 
+function render() {
+  document.body.textContent = state.count;
+}
+
+function dispatch(action){
+  state = changeState(state, action);
+  render();
+}
+
+
 let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
 changeState(state, action)
+
+render()
